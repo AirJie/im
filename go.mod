e6746414729ba374e9ba044dbd6bec927b07cd41
Liu <jian3.liu@ecarx.com.cn>
2020-04-14 18:26:00 +0800

    dailt commit

--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module github.com/aijie/michat
 go 1.14
 
 require (
+	github.com/golang/protobuf v1.3.5
+	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/gorilla/mux v1.7.4
+	github.com/gorilla/websocket v1.4.2
+	go.uber.org/zap v1.13.0
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20200103221440-774c71fcf114 // indirect
+	google.golang.org/grpc v1.26.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
--- ws_service/ws_conn.go
+++ ws_service/ws_conn.go
@@ -1,22 +1,36 @@
 package ws_service
 
-import "golang.org/x/net/websocket"
+import (
+	"github.com/aijie/michat/datas/pb"
+	"github.com/aijie/michat/logger"
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/status"
 
-type Link struct {
+	//"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
+	"log"
+)
+
+type Linker struct {
 	Conn     *websocket.Conn
 	AppId    int64
 	UserId   int64
 	DeviceId int64
-	Message chan *Message
+	Message  chan *Message
 }
 
-type Message struct {
-	UserId  string `json:"user_id"`
-	Message string `json:"message"`
-}
+type messageType int
 
-func NewLink(conn *websocket.Conn, appId, userId, deviceId int64) *Link {
-	return &Link{
+const (
+	UnknownType messageType = iota
+	SignInType
+	SyncType
+	HeartbeatType
+	MessageType
+)
+
+func NewLink(conn *websocket.Conn, appId, userId, deviceId int64) *Linker {
+	return &Linker{
 		Conn:     conn,
 		AppId:    appId,
 		UserId:   userId,
@@ -24,12 +38,64 @@ func NewLink(conn *websocket.Conn, appId, userId, deviceId int64) *Link {
 	}
 }
 
-func (lk *Link) DoConn() {
+func (r *Linker) DoConn() {
 	for {
-		lk.Conn.
-		//select {
-		//case msg := <- lk.Message:
-		//	//ws.SendMessage(msg.UserId, msg.Message)
-		//}
+		_, data, err := r.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		r.HandleMessage(data)
 	}
 }
+
+func (r *Linker) Heartbeat(input pb.Input) {
+	r.Output(pb.SessionType_Heartbeat, 0, nil, nil)
+}
+
+func (r *Linker) HandleMessage(data []byte) {
+	input := pb.Input{}
+	proto.Unmarshal(data, &input)
+	switch input.Type {
+	case pb.SessionType_Unknown:
+		log.Fatal("Get unknow type of message")
+	case pb.SessionType_Heartbeat:
+		r.Heartbeat(input)
+	case pb.SessionType_Sync:
+	case pb.SessionType_SignIn:
+	case pb.SessionType_MessageStream:
+	default:
+		logger.Sugar.Error("input type error")
+	}
+}
+
+func (r *Linker) Output(ptype pb.SessionType, requestId int64, err error, message proto.Message) {
+	var out = pb.Output{
+		Type: ptype,
+		Id:   requestId,
+	}
+	if err != nil {
+		status, _ := status.FromError(err)
+		out.Errcode = int32(status.Code())
+		out.Message = status.Message()
+	}
+
+	if message != nil {
+		out.Data, err = proto.Marshal(message)
+		if err != nil {
+			logger.Sugar.Error(err)
+			return
+		}
+	}
+	bytesData, err := proto.Marshal(message)
+	if err != nil {
+		logger.Sugar.Error(err)
+		return
+	}
+	err = r.Conn.WriteMessage(websocket.BinaryMessage, bytesData)
+	if err != nil {
+		logger.Sugar.Error(err)
+		return
+	}
+}
+
